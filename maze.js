c9134f9870dc8ce6064e622c998d93226ae1f11d
Rushane Barnes <barnesrushane@hotmail.com>
2018-10-13 01:39:54 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,8 +1,23 @@
-window.onload = function () {
+/*window.onload = function () { 
     document.getElementById("boundary1").addEventListener("mouseover", mouseOver);
     function mouseOver(){
         document.getElementById("boundary1").className = "boundary youlose";
+        //document.querySelectorAll("div.boundary");
     } 
+} */
+
+window.onload = function () {
+    var x = document.querySelectorAll(".boundary");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].addEventListener("mouseover", mouseOver);
+    }
+
+    function mouseOver() {
+        for (i = 0; i < x.length; i++) {
+            x[i].className = "boundary youlose";     
+        } 
+    }
 }
 
 
