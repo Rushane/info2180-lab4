7793674fdfdfc4e7a22caaff8338ed98c42011d6
Rushane Barnes <barnesrushane@hotmail.com>
2018-10-12 22:51:33 -0500

    Exercise 1

---
+++ maze.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    document.getElementById("boundary1").addEventListener("mouseover", mouseOver);
+    function mouseOver(){
+        document.getElementById("boundary1").className = "boundary youlose";
+    } 
+}
+
+
+
+
+
+
+
+
+
