6ceb5a1b67601c37ceca3615e14b91d64f1d16f8
Rushane Barnes <barnesrushane@hotmail.com>
2018-10-18 20:45:29 -0500

    Exercise 4

--- maze.js
+++ maze.js
@@ -1,11 +1,13 @@
 window.onload = function () {
     var x = document.querySelectorAll(".boundary");
     var y = document.getElementById("end");
+    var z = document.getElementById("start");
     var i;
     for (i = 0; i < x.length; i++) {
         x[i].addEventListener("mouseover", mouseOver);
     }
-    y.addEventListener("mouseover", mouseOver2)
+    y.addEventListener("mouseover", winMazeAlert) // event listener for alert 
+    z.addEventListener("click", resetMaze) // event listener for restarting
 
     function mouseOver() {
         for (i = 0; i < x.length; i++) {
@@ -13,8 +15,12 @@ window.onload = function () {
         }   
     }
 
-    function mouseOver2() {
+    function winMazeAlert() {
         alert("You win!");
+    }
+
+    function resetMaze() {
+        location.reload(); // location.reload() reloads the resource from the current URL
     }
 }
 
